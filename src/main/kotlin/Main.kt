9e82f2afe9d7fa2dfa651874fc3230a6a20a1df3
mackarpov <mackarpov@gmail.com>
2025-05-09 14:11:29 +0300

    Lesson 1 fineshed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,4 +2,19 @@ package org.example
 
 fun main() {
 
+    var numberOfOrders: Int = 42
+    val welcomeMessage: String = "Welcome to our app!"
+
+    println(numberOfOrders)
+    println(welcomeMessage)
+
+    numberOfOrders = 52
+    println(numberOfOrders)
+
+    // целочисленные: Int, Long (64 bit), Short, Byte (8 bit)
+    // целочисленные положительные: UInt
+    // вещественные: Double, Float (876.5f)
+    // строковые: String ("Hello")
+    // символьные: Char ('1')
+    // логические: Boolean (true, false)
 }
