e447deb1124d299c32c2c4c3b4b8d96a47d69408
mackarpov <mackarpov@gmail.com>
2025-05-10 20:26:09 +0300

    Lesson_10 Functions

---
+++ src/main/kotlin/Lesson_10.kt
@@ -0,0 +1,13 @@
+package org.example
+
+fun main() {
+    printNameAndAge(userName = getName(), userAge = getAge())
+}
+
+fun getName(): String? = readLine()
+
+fun getAge(): Int? = readLine()?.toInt()
+
+fun printNameAndAge(userName: String?, userAge: Int?) {
+    println("Данные пользователя: $userName, $userAge лет")
+}
