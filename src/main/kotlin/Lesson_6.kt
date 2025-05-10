0311c5b8d777c46ebc69c8f4e43b35da363869f1
mackarpov <mackarpov@gmail.com>
2025-05-10 15:53:26 +0300

    Lesson_6. Cycles

---
+++ src/main/kotlin/Lesson_6.kt
@@ -0,0 +1,17 @@
+package org.example
+// while, do while
+
+fun main() {
+
+    var counter = 5
+
+//    while (counter > 0){
+//        println("Реклама закончится через ${counter--}")
+//        Thread.sleep(1000)
+//    }
+
+    do {
+        println("Реклама закончится через ${counter--}")
+        Thread.sleep(1000)
+    } while (counter > 0)
+}
