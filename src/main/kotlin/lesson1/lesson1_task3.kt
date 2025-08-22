94ccb244a117876699392ba0a1d92bf99015a28f
mackarpov <mackarpov@gmail.com>
2025-08-22 23:24:30 +0300

    KS-1-3 lesson1 task3 solved

---
+++ src/main/kotlin/lesson1/lesson1_task3.kt
@@ -0,0 +1,21 @@
+package org.example.lesson1
+
+import kotlin.math.min
+
+fun main() {
+
+    val year: String = "1961"
+    var hour: String = "09"
+    var minute: String = "07"
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = "10"
+    minute = "55"
+
+    print(hour)
+    print(":")
+    print(minute)
+}
