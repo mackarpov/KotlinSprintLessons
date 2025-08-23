0f21800b0e4f5068f4f0651960d02d2316f65fb0
mackarpov <mackarpov@gmail.com>
2025-08-23 19:07:04 +0300

    KS-2-1 lesson2 task1 solved

---
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson2
+
+fun main() {
+
+    val score_1 = 3.0
+    val score_2 = 4.0
+    val score_3 = 3.0
+    val score_4 = 5.0
+    val averageScore = (score_1 + score_2 + score_3 + score_4) / 4
+
+    println(averageScore)
+}
