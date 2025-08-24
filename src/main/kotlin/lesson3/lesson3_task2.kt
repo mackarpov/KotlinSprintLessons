e2cdaecf5cc1df9ebceb3bc36aeee4077ed2f01f
mackarpov <mackarpov@gmail.com>
2025-08-25 02:20:46 +0300

    KS-3-3 lesson3 task2, solved

---
+++ src/main/kotlin/lesson3/lesson3_task2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson3
+
+fun main() {
+
+    var surName = "Андреева"
+    val name = "Татьяна"
+    val middleName = "Сергеевна"
+    var age = 20
+
+    println("$surName $name $middleName, $age")
+
+    surName = "Сидорова"
+    age = 22
+
+    println("$surName $name $middleName, $age")
+}
