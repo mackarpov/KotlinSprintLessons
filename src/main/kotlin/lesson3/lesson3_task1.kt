7144940809bac2c902d82530c7cc57bddab259a9
mackarpov <mackarpov@gmail.com>
2025-08-24 16:19:07 +0300

    KS-3-1 lesson3 task1 solved

---
+++ src/main/kotlin/lesson3/lesson3_task1.kt
@@ -0,0 +1,13 @@
+package org.example.lesson3
+
+fun main() {
+
+    val userName = "Alex"
+    var greetingMessage = "Good morning, $userName. Welcome to App!"
+
+    println(greetingMessage)
+
+    greetingMessage = "Good evening, $userName. Welcome to App!"
+
+    println(greetingMessage)
+}
