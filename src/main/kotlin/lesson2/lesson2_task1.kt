0991819f5c0afdc02e19e2ff09d66376440cb36e
mackarpov <mackarpov@gmail.com>
2025-08-24 23:12:40 +0300

    KS-2-1 lesson2 task1, added a variable

--- src/main/kotlin/lesson2/lesson2_task1.kt
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -2,11 +2,12 @@ package org.example.lesson2
 
 fun main() {
 
+    val numberOfStudents = 4
     val score_1 = 3.0
     val score_2 = 4.0
     val score_3 = 3.0
     val score_4 = 5.0
-    val averageScore = (score_1 + score_2 + score_3 + score_4) / 4
+    val averageScore = (score_1 + score_2 + score_3 + score_4) / numberOfStudents
 
     println(averageScore)
 }
