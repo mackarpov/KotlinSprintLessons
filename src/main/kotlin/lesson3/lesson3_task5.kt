a83973bd29e8f0841c80797cfc75394e240cfa9e
mackarpov <mackarpov@gmail.com>
2025-08-27 20:29:51 +0300

    KS-3-5 lesson3_task5.resolved

--- src/main/kotlin/lesson3/lesson3_task5.kt
+++ src/main/kotlin/lesson3/lesson3_task5.kt
@@ -3,9 +3,9 @@ package org.example.lesson3
 fun main() {
 
     val chessRecord = "D2-D4;0"
-    val cellFrom = "${chessRecord[0]}${chessRecord[1]}"
-    val cellTo = "${chessRecord[3]}${chessRecord[4]}"
-    val moveNumber = "${chessRecord[6]}"
+    val cellFrom = chessRecord.split('-', ';')[0]
+    val cellTo = chessRecord.split('-', ';')[1]
+    val moveNumber = chessRecord.split('-', ';')[2]
 
     println(cellFrom)
     println(cellTo)
