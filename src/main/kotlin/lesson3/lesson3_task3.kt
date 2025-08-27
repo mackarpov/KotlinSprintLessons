f89fd9458a97039197977f78b8c8a9dbe6d92458
mackarpov <mackarpov@gmail.com>
2025-08-27 20:03:08 +0300

    KS-3-3 lesson3_task3.resolved

--- src/main/kotlin/lesson3/lesson3_task3.kt
+++ src/main/kotlin/lesson3/lesson3_task3.kt
@@ -4,13 +4,18 @@ fun main() {
 
     val variable = 6
 
-    println("$variable * 1 = ${variable * 1}\n" +
-            "$variable * 2 = ${variable * 2}\n" +
-            "$variable * 3 = ${variable * 3}\n" +
-            "$variable * 4 = ${variable * 4}\n" +
-            "$variable * 5 = ${variable * 5}\n" +
-            "$variable * 6 = ${variable * 6}\n" +
-            "$variable * 7 = ${variable * 7}\n" +
-            "$variable * 8 = ${variable * 8}\n" +
-            "$variable * 9 = ${variable * 9}\n")
+    println(
+        """
+            $variable * 1 = ${variable * 1}
+            $variable * 2 = ${variable * 2}
+            $variable * 3 = ${variable * 3}
+            $variable * 4 = ${variable * 4}
+            $variable * 5 = ${variable * 5}
+            $variable * 6 = ${variable * 6}
+            $variable * 7 = ${variable * 7}
+            $variable * 8 = ${variable * 8}
+            $variable * 9 = ${variable * 9}
+            $variable * 10 = ${variable * 10}
+        """.trimIndent()
+    )
 }
