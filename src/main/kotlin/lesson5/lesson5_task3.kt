4d42ea9083ad9ae1f48229806c54ad3e08fdbe39
mackarpov <mackarpov@gmail.com>
2025-09-04 15:07:37 +0300

    KS-5-3 Задача: угадать 2 числа в диапазоне. Начало и конец диапазона выведены в константы.

--- src/main/kotlin/lesson5/lesson5_task3.kt
+++ src/main/kotlin/lesson5/lesson5_task3.kt
@@ -2,12 +2,12 @@ package org.example.lesson5
 
 fun main() {
 
-    println("Угадайте два числа от 0 до 42!")
+    println("Угадайте два числа от $MINIMUM_DIGIT до $MAXIMUM_DIGIT!")
 
-    print("Введите первое число от 0 до 42: ")
+    print("Введите первое число от $MINIMUM_DIGIT до $MAXIMUM_DIGIT: ")
     val firstNumber = readln().toInt()
 
-    print("Введите второе число от 0 до 42: ")
+    print("Введите второе число от $MINIMUM_DIGIT до $MAXIMUM_DIGIT: ")
     val secondNumber = readln().toInt()
 
     if ((firstNumber == FIRST_CORRECT_NUMBER || firstNumber == SECOND_CORRECT_NUMBER) &&
@@ -23,5 +23,7 @@ fun main() {
 
 }
 
+const val MINIMUM_DIGIT = 0
+const val MAXIMUM_DIGIT = 42
 const val FIRST_CORRECT_NUMBER = 7
 const val SECOND_CORRECT_NUMBER = 36
