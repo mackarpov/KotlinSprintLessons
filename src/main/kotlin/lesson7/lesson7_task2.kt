4afcedeb9ed2bd352f68abb387817907b8f3b92f
mackarpov <mackarpov@gmail.com>
2025-09-17 21:47:16 +0300

    KS-7-2 Авторизация. Оптимизировал .shuffled на .random, вынес переменные из цикла

--- src/main/kotlin/lesson7/lesson7_task2.kt
+++ src/main/kotlin/lesson7/lesson7_task2.kt
@@ -2,13 +2,13 @@ package org.example.lesson7
 
 fun main() {
 
+    val authorizationCode = (1000..9999).random()
+    println("Ваш код авторизации: $authorizationCode")
+    var userCode: Int
+
     do {
-        val authorizationCode = (1000..9999).shuffled()[0]
-        println("Ваш код авторизации: $authorizationCode")
-
         print("Введите код авторизации: ")
-        val userCode = readln().toInt()
-
+        userCode = readln().toInt()
     } while (userCode != authorizationCode)
     println("Вы авторизировались!")
 }
