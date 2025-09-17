51ae5c9067dcfbddde7e969e47ea37d8833fabb5
mackarpov <mackarpov@gmail.com>
2025-09-17 21:29:37 +0300

    KS-7-1 6-ти зн.пароль. Исправлен диапазон. Заменил size на indices, tostring на интерполяцию.

--- src/main/kotlin/lesson7/lesson7_task1.kt
+++ src/main/kotlin/lesson7/lesson7_task1.kt
@@ -2,14 +2,12 @@ package org.example.lesson7
 
 fun main() {
 
-    val rangeOfDigits = (1..9).shuffled().take(3)
+    val rangeOfDigits = (0..9).shuffled().take(3)
     val rangeOfChars = ('a'..'z').shuffled().take(3)
     var password = ""
 
-    for (i in FIRST_INDEX_IN_RANGE until rangeOfChars.size) {
-        password = password + rangeOfChars[i].toString() + rangeOfDigits[i].toString()
+    for (i in rangeOfChars.indices) {
+        password = password + "${rangeOfChars[i]}" + "${rangeOfDigits[i]}"
     }
     println(password)
 }
-
-private const val FIRST_INDEX_IN_RANGE = 0
