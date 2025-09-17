06832f2ea29906260524708a6a84141a01dc4a3a
mackarpov <mackarpov@gmail.com>
2025-09-17 21:53:15 +0300

    KS-7-4 Выполнен. Пробел добавлен.

--- src/main/kotlin/lesson7/lesson7_task4_Timer.kt
+++ src/main/kotlin/lesson7/lesson7_task4_Timer.kt
@@ -5,7 +5,7 @@ fun main() {
     print("Введите количество секунд для таймера: ")
     val amountOfSeconds = readln().toInt()
 
-    for (i in amountOfSeconds downTo 0){
+    for (i in amountOfSeconds downTo 0) {
         Thread.sleep(1000)
         println("До конца осталось: $i секунд")
     }
