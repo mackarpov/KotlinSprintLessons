49fd947c5375d0103137b7b3f1472061f80e55fb
mackarpov <mackarpov@gmail.com>
2025-09-18 09:36:01 +0300

    KS-7-2. Исправлено. Генерация кода внесена в цикл.

--- src/main/kotlin/lesson7/lesson7_task2.kt
+++ src/main/kotlin/lesson7/lesson7_task2.kt
@@ -2,13 +2,16 @@ package org.example.lesson7
 
 fun main() {
 
-    val authorizationCode = (1000..9999).random()
-    println("Ваш код авторизации: $authorizationCode")
+    var authorizationCode: Int
     var userCode: Int
 
     do {
+        authorizationCode = (1000..9999).random()
+        println("Ваш код авторизации: $authorizationCode")
+
         print("Введите код авторизации: ")
         userCode = readln().toInt()
     } while (userCode != authorizationCode)
+
     println("Вы авторизировались!")
 }
