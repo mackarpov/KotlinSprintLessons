8a7089dad2468df836d52d95fe94cba6e4b66659
mackarpov <mackarpov@gmail.com>
2025-09-18 20:48:36 +0300

    KS-7-5. Исправлено. Добавил проверку длины пароля. Пароль содержит все типы символов.

--- src/main/kotlin/lesson7/lesson7_task5_ComplexPasswords.kt
+++ src/main/kotlin/lesson7/lesson7_task5_ComplexPasswords.kt
@@ -2,20 +2,43 @@ package org.example.lesson7
 
 fun main() {
 
-    print("Введите количество знаков в пароле (не меньше $MIN_SIZE_OF_PASSWORD): ")
-    val numberOfCharacters = readln().toInt()
+    var amountOfCharacters: Int
 
-    val digitTable = (0..9).shuffled().take(numberOfCharacters)
-    val lowercaseLettersTable = ('a'..'z').shuffled().take(numberOfCharacters)
-    val capitalLettersTable = ('A'..'Z').shuffled().take(numberOfCharacters)
-    val complexPassword =
-        (digitTable + lowercaseLettersTable + capitalLettersTable).shuffled().take(numberOfCharacters)
-    var finalPassword = ""
+    do {
+        print("Введите количество знаков в пароле (не меньше $MIN_SIZE_OF_PASSWORD): ")
+        amountOfCharacters = readln().toInt()
 
-    for (i in 0 until complexPassword.size) {
-        finalPassword += complexPassword[i]
+        if (amountOfCharacters < MIN_SIZE_OF_PASSWORD) {
+            println("Ваш пароль меньше $MIN_SIZE_OF_PASSWORD символов, пропробуйте еще раз.")
+        }
+    } while (amountOfCharacters < MIN_SIZE_OF_PASSWORD)
+
+    val digitTable = 0..9
+    val lowercaseLettersTable = 'a'..'z'
+    val capitalLettersTable = 'A'..'Z'
+    var complexPassword: String = digitTable.random().toString() +
+            lowercaseLettersTable.random() + capitalLettersTable.random()
+
+    while (true) {
+        if (complexPassword.length < amountOfCharacters) {
+            complexPassword += digitTable.random().toString()
+        } else {
+            break
+        }
+
+        if (complexPassword.length < amountOfCharacters) {
+            complexPassword += lowercaseLettersTable.random()
+        } else {
+            break
+        }
+
+        if (complexPassword.length < amountOfCharacters) {
+            complexPassword += capitalLettersTable.random()
+        } else {
+            break
+        }
     }
-    println(finalPassword)
+    println(complexPassword)
 }
 
 const val MIN_SIZE_OF_PASSWORD = 6
