b85ff2b10ad54fc94a85bbf35abcaa7e3b122957
mackarpov <mackarpov@gmail.com>
2025-09-18 21:48:06 +0300

    KS-7-5. Сложный пароль. Изменил проверку длины. Алгоритм изменен на цикл for(i..)

--- src/main/kotlin/lesson7/lesson7_task5_ComplexPasswords.kt
+++ src/main/kotlin/lesson7/lesson7_task5_ComplexPasswords.kt
@@ -2,41 +2,23 @@ package org.example.lesson7
 
 fun main() {
 
-    var amountOfCharacters: Int
+    print("Введите количество знаков в пароле (не меньше $MIN_SIZE_OF_PASSWORD): ")
+    val amountOfCharacters = readln().toInt()
 
-    do {
-        print("Введите количество знаков в пароле (не меньше $MIN_SIZE_OF_PASSWORD): ")
-        amountOfCharacters = readln().toInt()
+    if (amountOfCharacters < MIN_SIZE_OF_PASSWORD) {
+        println("Ваш пароль меньше $MIN_SIZE_OF_PASSWORD символов, пропробуйте еще раз.")
+        return
+    }
 
-        if (amountOfCharacters < MIN_SIZE_OF_PASSWORD) {
-            println("Ваш пароль меньше $MIN_SIZE_OF_PASSWORD символов, пропробуйте еще раз.")
-        }
-    } while (amountOfCharacters < MIN_SIZE_OF_PASSWORD)
+    val digitsRange = 0..9
+    val lowercaseLettersRange = 'a'..'z'
+    val capitalLettersRange = 'A'..'Z'
+    val complexRangeOfSymbols = (digitsRange + lowercaseLettersRange + capitalLettersRange).shuffled()
+    var complexPassword = digitsRange.random().toString() +
+            lowercaseLettersRange.random() + capitalLettersRange.random()
 
-    val digitTable = 0..9
-    val lowercaseLettersTable = 'a'..'z'
-    val capitalLettersTable = 'A'..'Z'
-    var complexPassword: String = digitTable.random().toString() +
-            lowercaseLettersTable.random() + capitalLettersTable.random()
-
-    while (true) {
-        if (complexPassword.length < amountOfCharacters) {
-            complexPassword += digitTable.random().toString()
-        } else {
-            break
-        }
-
-        if (complexPassword.length < amountOfCharacters) {
-            complexPassword += lowercaseLettersTable.random()
-        } else {
-            break
-        }
-
-        if (complexPassword.length < amountOfCharacters) {
-            complexPassword += capitalLettersTable.random()
-        } else {
-            break
-        }
+    for (i in 4..amountOfCharacters) {
+        complexPassword += complexRangeOfSymbols[i]
     }
     println(complexPassword)
 }
