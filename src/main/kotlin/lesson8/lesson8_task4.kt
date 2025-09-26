4dff1591c4cf20b2d535204e2faaf72d788aaea8
mackarpov <mackarpov@gmail.com>
2025-09-27 01:42:12 +0300

    KS-8-4 Замена ингредиента. Убрал цикл for (in...)

--- src/main/kotlin/lesson8/lesson8_task4.kt
+++ src/main/kotlin/lesson8/lesson8_task4.kt
@@ -3,22 +3,22 @@ package org.example.lesson8
 fun main() {
 
     val arrayOfIngredients = arrayOf("укроп", "киви", "шпинат", "минералка", "манго")
+    val newUserIngredient: String
 
     for (i in arrayOfIngredients) println(i)
 
     print("Какой ингредиент вы хотите заменить? ")
     val userIngredient = readln()
+    val indexOfIngredient = arrayOfIngredients.indexOf(userIngredient)
 
-    if (userIngredient !in arrayOfIngredients) {
+    if (indexOfIngredient == -1) {
         println("Такого ингредиента нет в списке")
         return
+    } else {
+        print("Какой ингредиент вы хотите добавить? ")
+        newUserIngredient = readln()
+        arrayOfIngredients[indexOfIngredient] = newUserIngredient
     }
-
-    print("Какой ингредиент вы хотите добавить? ")
-    val newUserIngredient = readln()
-    val indexOfNewIngredient = arrayOfIngredients.indexOf(userIngredient)
-
-    arrayOfIngredients.set(indexOfNewIngredient, newUserIngredient)
 
     println("Готово! Вы сохранили следующий список:")
     for (i in arrayOfIngredients) println(i)
